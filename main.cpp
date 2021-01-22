66ebe2566d0a86c3e80f1cf95fccda7d314b1108
Siarhei <sn.kapitonenka@pg7.so-net.ne.jp>
2021-01-23 00:10:20 +0900

    all files

--- main.cpp
+++ main.cpp
@@ -3,31 +3,36 @@
 #include "EthernetInterface.h" // NTP
 #include "ntp-client/NTPClient.h" // NTP
 
-// RTC module pins
+// NTP 
+time_t timestamp;
+
+// RTC module ->
 #define SCLK    D13 // CLK
 #define IO      D12 // DATA
 #define CE      D8 // RST
+const char *swd[8] = { "", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" }; 
+const char *smth[13] = {"", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"}; 
+struct tm * timeinfo;
+unsigned char day;
+unsigned char mth;
+unsigned char year;
+unsigned char dow;
+unsigned char hr;
+unsigned char minu;
+unsigned char sec;
 ds1302 clk(SCLK, IO, CE);
+// <- RTC module
 
-int main() 
+void RTC_PrintDateTime()
 {
-    //DS1302 RTC Module -->
-    const char *swd[8] = { "Undef", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
-    clk.init();
-    bool bSynClock = false;
-    struct tm * timeinfo;
-    unsigned char day;
-    unsigned char mth;
-    unsigned char year;
-    unsigned char dow;
-    unsigned char hr;
-    unsigned char min;
-    unsigned char sec;
     clk.get_date(day, mth, year, dow);
-    clk.get_time(hr, min, sec);
-    printf("RTC module date and time: %s %d/%d/%d  %d:%d:%d \r\n", swd[dow], day, mth, year, hr, min, sec);
-    // <-- DS1302 RTC Module
+    clk.get_time(hr, minu, sec);
+    printf("RTC module date and time: %s, %s %d, %d  %d:%d:%d \r\n", swd[dow], smth[mth], day, year + 2000, hr, minu, sec);   
+}
 
+void RTC_Synch()
+{
+     //NTP Client ->
     printf("NTP Client example (using Ethernet)\r\n");
     EthernetInterface eth;
     eth.connect();
@@ -38,41 +43,35 @@ int main()
     
     NTPClient ntp(&eth);
     
-    while(1) 
+    timestamp = ntp.get_timestamp();
+    timestamp = timestamp + 3600 * 9; // Japan Time +9 hours offset
+    if (timestamp > 3820313209) 
     {
-        time_t timestamp = ntp.get_timestamp();
-        timestamp = timestamp + 3600 * 9; // Japan Time +9 hours offset
-        if (timestamp < 0) {
-            printf("An error occurred when getting the time. Code: %u\r\n", timestamp);
-        } 
-        else 
-        {
-            printf("Current time is %s\r\n", ctime(&timestamp));
-            if (!bSynClock)
-            {
-                bSynClock   = true;
-                timeinfo    = localtime(&timestamp);
-                day         = (unsigned char) timeinfo -> tm_mday;
-                mth         = (unsigned char) (timeinfo -> tm_mon + 1);
-                year        = (unsigned char) (timeinfo -> tm_year + 1900 - 2000);
-                dow         = (unsigned char) (timeinfo -> tm_wday + 1);
-                hr          = (unsigned char) timeinfo -> tm_hour;
-                min         = (unsigned char) timeinfo -> tm_min;
-                sec         = (unsigned char) timeinfo -> tm_sec;
-                clk.set_datetime(day, mth, year, dow, hr, min, sec);
-                printf("RTC module date and time: %s %d/%d/%d  %d:%d:%d synchronized with NTP sertver.\r\n", swd[dow], day, mth, year, hr, min, sec);
-            }
-            else 
-            {
-                clk.get_date(day, mth, year, dow);
-                clk.get_time(hr, min, sec);
-                printf("RTC module date and time: %s %d/%d/%d  %d:%d:%d \r\n", swd[dow], day, mth, year, hr, min, sec);
-            }
-        }
-        
-        printf("Waiting 10 seconds before trying again...\r\n");
-       // wait(10.0);  
-       ThisThread::sleep_for(10s);      
+        printf("An error occurred when getting the time. Timestamp %u sec since 1900 must be > 3820313209\r\n", timestamp);
+    } 
+    else 
+    {
+        printf("Current time is %s\r\n", ctime(&timestamp)); 
+        timeinfo    = localtime(&timestamp);
+        day         = (unsigned char) timeinfo -> tm_mday;
+        mth         = (unsigned char) (timeinfo -> tm_mon + 1);
+        year        = (unsigned char) (timeinfo -> tm_year + 1900 - 2000);
+        dow         = (unsigned char) (timeinfo -> tm_wday + 1);
+        hr          = (unsigned char) timeinfo -> tm_hour;
+        minu        = (unsigned char) timeinfo -> tm_min;
+        sec         = (unsigned char) timeinfo -> tm_sec;
+        clk.set_datetime(day, mth, year, dow, hr, minu, sec);
+        printf("RTC module date and time: %s, %s %d, %d  %d:%d:%d synchronized with NTP sertver.\r\n", swd[dow], smth[mth], day, year + 2000, hr, minu, sec);   
     }
+}
+
+int main() 
+{
+    //DS1302 RTC Module -->
+    clk.init();
+    RTC_PrintDateTime();
+    RTC_Synch();
+    RTC_PrintDateTime();
+    // <-- DS1302 RTC Module
 }
 
